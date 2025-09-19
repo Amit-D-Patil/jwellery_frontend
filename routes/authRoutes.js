1dddd617b5f261093378e886a56f250dadabfcdf
Amit Patil <124231532+Amit-D-Patil@users.noreply.github.com>
2025-09-19 19:33:16 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,32 +8,41 @@ const router = express.Router();
 const USERS_FILE = './data/users.json';
 
 // Register a new user
-router.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
+// router.post('/register', async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
 
-    if (!username || !password) {
-      return res.status(400).json({ message: 'Username and password are required' });
-    }
+//     if (!username || !password) {
+//       return res.status(400).json({ message: 'Username and password are required' });
+//     }
 
-    // const usersData = await readFile(USERS_FILE, 'utf-8');
-    // const users = JSON.parse(usersData);
+//     // const usersData = await readFile(USERS_FILE, 'utf-8');
+//     // const users = JSON.parse(usersData);
 
-    // if (users.find(user => user.username === username)) {
-    //   return res.status(400).json({ message: 'User already exists' });
-    // }
+//     // if (users.find(user => user.username === username)) {
+//     //   return res.status(400).json({ message: 'User already exists' });
+//     // }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log({ username, password: hashedPassword });
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     console.log({ username, password: hashedPassword });
 
-    // await writeFile(USERS_FILE, JSON.stringify(users, null, 2));
+//     // await writeFile(USERS_FILE, JSON.stringify(users, null, 2));
 
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+//     res.status(201).json({ message: 'User registered successfully' });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+const users = [
+  {
+    "username": "nkop",
+    "password": "$2b$10$qMJM9P9PlVcG2jeuETTe4OSVykorJt8Qyqe1Dh1UBi0Z50ae8b8Y6"
+  },
+  {
+    username: 'admin',
+    password: '$2b$10$8EEOmy050inyO8yDf48QSuDPqjiMePWG2hHtq9EwN.JXNY/HYIpZS'
   }
-});
-
+]
 // Login a user
 router.post('/login', async (req, res) => {
   try {
@@ -42,17 +51,6 @@ router.post('/login', async (req, res) => {
     if (!username || !password) {
       return res.status(400).json({ message: 'Username and password are required' });
     }
-
-    const users = [
-      {
-        "username": "nkop",
-        "password": "$2b$10$qMJM9P9PlVcG2jeuETTe4OSVykorJt8Qyqe1Dh1UBi0Z50ae8b8Y6"
-      },
-      {
-        username: 'admin',
-        password: '$2b$10$8EEOmy050inyO8yDf48QSuDPqjiMePWG2hHtq9EwN.JXNY/HYIpZS'
-      }
-    ]
 
 
     const user = users.find(user => user.username === username);
@@ -83,8 +81,6 @@ import { authenticateToken } from '../middleware/auth.js';
 // create /verify route to verify JWT token it will be a get route that takes token as bearer token in header
 router.get('/verify', authenticateToken, async (req, res) => {
   try {
-    const usersData = await readFile(USERS_FILE, 'utf-8');
-    const users = JSON.parse(usersData);
     const user = users.find(u => u.username === req.user.username);
 
     if (!user) {
